7b2360f43d8d3dce25324eeba6d896de15562429
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-04-30 20:59:00 +0430

    JTable component

---
+++ components/JTable/JTable.vue
@@ -0,0 +1,21 @@
+<template>
+  <table>
+    <JTableRow v-for="(values, index) in items" :key="index" :values="values" />
+  </table>
+</template>
+
+<script>
+import JTableRow from "@naserahadi/j-table-row/JTableRow";
+export default {
+  name: "JTable",
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    JTableRow,
+  },
+};
+</script>
